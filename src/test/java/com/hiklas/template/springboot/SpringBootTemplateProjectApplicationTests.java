4991f3f36ff1fc5bb48c8b35ad8f3b17d265181b
Fiona Bianchi <fiona@hiklas.co.uk>
2023-10-10 15:46:33 +0100

    Adding template code

---
+++ src/test/java/com/hiklas/template/springboot/SpringBootTemplateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiklas.template.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
